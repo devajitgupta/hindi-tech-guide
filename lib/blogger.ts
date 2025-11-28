a8fe8b520e744f14eee0b32326047ee4a40c36ef
ajit <yourajitgupta@gmail.com>
2025-11-28 16:54:32 +0530

    ff

--- lib/blogger.ts
+++ lib/blogger.ts
@@ -1,22 +1,8 @@
 const BLOG_ID = process.env.BLOG_ID!;
 const API_KEY = process.env.BLOGGER_API_KEY!;
 const BASE_URL = `https://www.googleapis.com/blogger/v3/blogs/${BLOG_ID}`;
-import { unified } from "unified";
-import rehypeParse from "rehype-parse";
-import rehypeStringify from "rehype-stringify";
-import rehypeSanitize from "rehype-sanitize";
 
-import { visit } from "unist-util-visit";
-export function transformSpansToParagraphs() {
-  return (tree: any) => {
-    visit(tree, "element", (node) => {
-      if (node.tagName === "span" && node.properties?.style?.includes("font-size")) {
-        node.tagName = "p";
-        delete node.properties?.style;
-      }
-    });
-  };
-}
+
 
 export async function getAllPosts(limit: number = 20) {
   const res = await fetch(
@@ -45,15 +31,4 @@ export async function getAllSlugs() {
     const slug = post.url.split("/").pop()?.replace(".html", "")
     return { slug }
   })
-}export async function cleanHtml(html: string) {
-  const file = await unified()
-    .use(rehypeParse, { fragment: true }) // parse HTML fragment
-    
-    .use(rehypeSanitize) // remove inline styles / unsafe tags
-        .use(transformSpansToParagraphs) // <---- use it here
-
-    .use(rehypeStringify)
-    .process(html);
-
-  return String(file);
 }
